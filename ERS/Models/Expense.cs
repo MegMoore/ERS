5327544338d4444a1c536052c6ae3d998b385d43
David Benkner <d.benkner@gmail.com>
2023-09-01 12:01:07 -0400

    Table for expense and item created

---
+++ ERS/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ERS.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Column(TypeName ="decimal(11,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ ERS/Models/Expenselines.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ERS.Models
+{
+    public class Expenselines
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int ExpenseId { get; set; }
+        public virtual Expense? Expense { get; set; }
+        public int ItemId { get; set; }
+        [JsonIgnore]
+        public virtual Item? Item { get; set;}
+
+    }
+}
